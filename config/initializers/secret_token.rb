bbf5dc268ef1b2397f168a83b3b53aef6402797e
Meredith Underell <m.underell@gmail.com>
2013-08-20 08:50:28 -0700

    First commit, setup gem dependencies and run rspec generator

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EventProject::Application.config.secret_token = 'eb46308008e24db8d5c417c47dc786c35d3b2d52632d6a41373930595fdcfa2539966fba93853c73dbcc6058742f9ed87b775a2c1061e2318ad040647dd57741'
